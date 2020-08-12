3cb680fabb0d93e011b02018382e71786bf92f81
Elijah Kim <elijahkim@Elijahs-MacBook-Pro.local>
2020-08-12 12:08:03 -0400

    Finished most of it, working on likeSection

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -9,6 +9,15 @@ const Posts = (props) => {
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {
+        posts.map(posting => {
+          return <Post
+            key={posting.id}
+            post={posting}
+            likePost={posting.likes}
+          />
+        })
+      }
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
